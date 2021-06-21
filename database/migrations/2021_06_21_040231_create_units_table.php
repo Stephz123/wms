46409af2443bb54d447f2f175de510b59338248f
stephz123 <st3phzz87@yahoo.com>
2021-06-21 11:25:17 +0700

    add table units

---
+++ database/migrations/2021_06_21_040231_create_units_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUnitsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('units', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name',10);
+            $table->string('create_by',50);
+            $table->string('update_by',50);
+            $table->timestamps();
+            $table->string('delete_by',50);
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('units');
+    }
+}
